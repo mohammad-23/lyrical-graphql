62b7fa0d02f325186c4144a61e05d8466effed6e
mohammad-23 <mohammad8758@hotmail.com>
2020-11-04 22:10:01 +0530

    Remove mongo uri

--- server/server.js
+++ server/server.js
@@ -8,8 +8,7 @@ const schema = require("./schema/schema");
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI =
-  "mongodb+srv://mohammadUser:4A96jssq2RXyCOEA@cluster0.jn0zp.mongodb.net/lyrical?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoLab URI");
